43fecea3bd529785a5d5946705d2b602973caaf8
Sandro Almeida <sandro001@gmail.com>
2020-01-30 17:48:57 -0300

    Move HttpClientModule dependency to photos.module

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
     declarations: [ PhotoComponent ], //these are the components that are part of this module
-    exports: [ PhotoComponent ]       //these are the components that are exported from this module
+    exports: [ PhotoComponent ],       //these are the components that are exported from this module
+    imports: [ HttpClientModule ]
 })
 export class PhotosModule {}
