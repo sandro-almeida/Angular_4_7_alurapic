0d0da28c18e83026a33900391290b45f28f7e100
Sandro Almeida <sandro001@gmail.com>
2020-01-30 18:15:15 -0300

    Create new component using Angula CLI - ng g c <component>

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,9 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ], //these are the components that are part of this module
+    declarations: [ PhotoComponent, PhotoListComponent ], //these are the components that are part of this module
     exports: [ PhotoComponent ],       //these are the components that are exported from this module
     imports: [ HttpClientModule ]
 })
