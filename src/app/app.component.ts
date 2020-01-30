672fdf24d59719798fd83dc02c0ba77881831513
Sandro Almeida <sandro001@gmail.com>
2020-01-30 17:44:42 -0300

    Component life cycle - implementing interface OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,28 @@
 import { PhotoService } from './photos/photo/photo.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  
   photos: Object[] = [];
-
+  
   constructor(
-    photoService: PhotoService
-  ) {
-      photoService
-        .listFromUser('flavio')
-        .subscribe(photos => {  //with subscribe then data is in fact retrieved
-          this.photos = photos;
-          console.log('photos', this.photos);
-        });
+    private photoService: PhotoService
+  ) {}
+    
+    ngOnInit(): void {
+      this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => {  //with subscribe then data is in fact retrieved
+        this.photos = photos;
+        console.log('photos', this.photos);
+      });
+    }
+    
+    
   }
-
-}
+  
