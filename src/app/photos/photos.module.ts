447daf31ca474915bd9337e64bf3f22593890888
Sandro Almeida <sandro001@gmail.com>
2020-01-30 18:51:55 -0300

    Include CommonModule as an import module on photos.module

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
     declarations: [ PhotoComponent, PhotoListComponent ], //these are the components that are part of this module
     //exports: [ PhotoComponent ],       //these are the components that are exported from this module
-    imports: [ HttpClientModule ]
+    imports: [ HttpClientModule, CommonModule ]
 })
 export class PhotosModule {}
