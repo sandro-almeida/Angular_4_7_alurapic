2713f62b71152bf25c3c0aa23135e63995c96e50
Sandro Almeida <sandro001@gmail.com>
2020-01-31 11:50:00 -0300

    Create routing module - app.routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { PhotosModule } from './photos/photos.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
