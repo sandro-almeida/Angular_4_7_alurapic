9ac3da1fc649a96994a2e14cd9caac038073639f
Sandro Almeida <sandro001@gmail.com>
2020-01-31 12:39:24 -0300

    Parametrization of routes using ActivatedRoute

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,12 +12,16 @@ export class PhotoListComponent implements OnInit {
   photos: Object[] = [];
   
   constructor(
-    private photoService: PhotoService
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
   ) {}
     
     ngOnInit(): void {
+
+      const userName = this.activatedRoute.snapshot.params.userName;
+
       this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe(photos => {  //with subscribe then data is in fact retrieved
         this.photos = photos;
         console.log('photos', this.photos);
