1729c6fe41d22bd1e368aa131d0d9ff12a133469
Sandro Almeida <sandro001@gmail.com>
2020-01-31 18:23:28 -0300

    Use Subscribe from Rxjs

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from './../photo/photo';
 
@@ -8,18 +10,26 @@ import { Photo } from './../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
-
+export class PhotoListComponent implements OnInit, OnDestroy {
+  
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
   
   constructor(
     private activatedRoute: ActivatedRoute
   ) {}
     
-    ngOnInit(): void {
-      //snapshot.data allows us to access the value of photos from app.routing.module.ts
-      this.photos = this.activatedRoute.snapshot.data['photos'];
-    } 
-
+  ngOnInit(): void {
+    //snapshot.data allows us to access the value of photos from app.routing.module.ts
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+    .pipe(debounceTime(300))  //300 ms; means that all the emitted values will be ignored, being considered only after 300 ms  
+    .subscribe(filter => this.filter = filter); //subscribe will listen for the emitted value and assign it to this.filter
+    
+  } 
+  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
 }
