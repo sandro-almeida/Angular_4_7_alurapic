53ca7685f9f7d282f776ffd2ed4475df5d89e178
Sandro Almeida <sandro001@gmail.com>
2020-01-31 14:21:30 -0300

    Group 3 photos per row

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -9,10 +9,22 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[] = [];
+  rows : any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.rows = this.groupColumns(this.photos);
   }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];  //array that will contain arrays with 3 photos each
+    const NUMBER_OF_PHOTOS_PER_ROW: number = 3;
+
+    for(let index = 0; index < photos.length; index+=NUMBER_OF_PHOTOS_PER_ROW) {
+        newRows.push(photos.slice(index, index + NUMBER_OF_PHOTOS_PER_ROW));
+    }
+    return newRows;
+}
 
 }
