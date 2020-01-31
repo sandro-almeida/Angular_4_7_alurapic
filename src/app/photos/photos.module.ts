7f091f6f9f689a9f3e4e80eb37d0d47d83114022
Sandro Almeida <sandro001@gmail.com>
2020-01-31 11:06:43 -0300

    Create new component photo-form via Angular CLI

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations: [ PhotoComponent, PhotoListComponent ], //these are the components that are part of this module
+    declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent ], //these are the components that are part of this module
     //exports: [ PhotoComponent ],       //these are the components that are exported from this module
     imports: [ HttpClientModule, CommonModule ]
 })
