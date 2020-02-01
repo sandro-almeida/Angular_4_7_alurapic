43a3b80bbc778c44d06f0b922497743cf0133b88
Sandro Almeida <sandro001@gmail.com>
2020-02-01 19:09:19 -0300

    Output property with custom event

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
@@ -8,11 +8,21 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class SearchComponent implements OnInit, OnDestroy {
 
+    /* Output property step-by-step
+    1) photo-list.component.html declares a customized event (onTyping) in ap-search tag.
+    2) search.component.ts declares an @Output() property with the same name of the customized event (onTyping) above which receives an EventEmitter (from @angular/core).
+    3) search.component.ts emits an event that will be read by photo-list.component.html.
+    4) emitted value from search.component.ts will fall into $event in photo-list.component.html
+    */
+    @Output() onTyping = new EventEmitter<string>();
+    @Input() valorDoInput: string = '';
+
     debounce: Subject<string> = new Subject<string>();
 
     ngOnInit(): void {
         this.debounce
-        .pipe(debounceTime(300));  //300 ms; means that all the emitted values will be ignored, being considered only after 300 ms  
+        .pipe(debounceTime(300))  //300 ms; means that all the emitted values will be ignored, being considered only after 300 ms  
+        .subscribe(filter => this.onTyping.emit(filter));  //event being emitted -> step 3
 //        .subscribe(filter => this.filter = filter); //subscribe will listen for the emitted value and assign it to this.filter
     } 
 
